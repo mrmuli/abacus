6d9dcf5eb8a6e280e32d9cc763b9f5084469fd95
alex-magana <alex.magana@andela.com>
2018-08-24 22:32:44 +0300

    Add support for multiplication

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,4 +1,10 @@
 class Compute:
-	def __init__(self, operator, operand):
-		self.operator = operator
-		self.operands = operands
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def multiply(self):
+        sum  = 1
+        for item in self.operands:
+            sum *= item
+        print(sum)
